d7b1d6aa93cfbcbd49eb39996d2cbe9ef47f7b65
abbie930 <abbiecheng930@gmail.com>
2023-05-08 18:54:08 -0400

    fix: fix error

--- src/components/Player.js
+++ src/components/Player.js
@@ -34,21 +34,15 @@ const Player = ({ currentSong, isPlaying, setIsPlaying}) => {
     setSongInfo({ ...songInfo, currentTime: e.target.value })
   }
   //State
-  const[songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0
   })
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input 
-          min={0} 
-          max={songInfo.duration} 
-          value={songInfo.currentTime}
-          onChange={dragHandler}
-          type="range" 
-        />
+        <input min={0} max={songInfo.duration} value={songInfo.currentTime} onChange={dragHandler} type="range" />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
