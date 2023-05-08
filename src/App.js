17ea7526f659c5666b8aec72df8a02d448fdeb44
abbie930 <abbiecheng930@gmail.com>
2023-05-08 17:27:37 -0400

    feat: add useState for songs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react"
 //Import Styles
 import "./styles/app.scss"
+//Import data
+import data from "./data"
 
 //Adding Components
-import Player from './components/Player'
-import Song from './components/Song'
+import Player from "./components/Player"
+import Song from "./components/Song"
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data())
+  const [currentSong,setCurrentSong] = useState(songs[0])
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   )
