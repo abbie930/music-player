cac31da13c32af3fc5c27f77a1c79bc634d07528
abbie930 <abbiecheng930@gmail.com>
2023-05-08 13:53:21 -0400

    feat: add song and player components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+//Adding Components
+import Player from './components/Player'
+import Song from './components/Song'
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
