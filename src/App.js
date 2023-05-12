c6a5f06b57896e69648c93c3a9fd408beb1302c6
abbie930 <abbiecheng930@gmail.com>
2023-05-12 15:59:05 -0400

    feat: add dark/light mode switch functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { ThemeContext } from "./contexts/ThemeContext"
 //Import Styles
 import "./styles/app.scss"
 //Import data
@@ -16,32 +17,48 @@ function App() {
   const [currentSong, setCurrentSong] = useState(songs[0])
   const [isPlaying, setIsPlaying] = useState(false)
   const [libraryStatus, setLibraryStatus] = useState(false)
+
+  //
+  const isBrowserDefaulDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  const getDefaultTheme = () => {
+    const localStorageTheme = localStorage.getItem('default-theme')
+    const browserDefault = isBrowserDefaulDark() ? 'dark' : 'light'
+    return localStorageTheme || browserDefault
+  }
+
+  const [theme, setTheme] = useState(getDefaultTheme())
+
   return (
-    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
-      <Nav 
-        libraryStatus={libraryStatus} 
-        setLibraryStatus={setLibraryStatus} 
-      />
-      <Song 
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-      />
-      <Player 
-        setIsPlaying={setIsPlaying} 
-        isPlaying={isPlaying} 
-        currentSong={currentSong}
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-      />
-      <Library
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        isPlaying={isPlaying}
-        setSongs={setSongs}
-        currentSong={currentSong}
-        libraryStatus={libraryStatus}
-      />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`theme-${theme}`}>
+        <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+          <Nav 
+            libraryStatus={libraryStatus} 
+            setLibraryStatus={setLibraryStatus} 
+          />
+          <Song 
+            currentSong={currentSong}
+            isPlaying={isPlaying}
+          />
+          <Player 
+            setIsPlaying={setIsPlaying} 
+            isPlaying={isPlaying} 
+            currentSong={currentSong}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+          />
+          <Library
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+            currentSong={currentSong}
+            libraryStatus={libraryStatus}
+          />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
